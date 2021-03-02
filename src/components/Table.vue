e0e14c1d8f0e726b1b12604ee17596a7f360034c
Youri Seichter <yseichter@gmail.com>
2021-03-02 18:41:54 +0100

    update visuals

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,12 +1,11 @@
 <template>
-  <div>
-      <!-- class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-r sm:text-sm border-gray-100 m-2 p-2" -->
+  <div class="block w-full">
     <input
       type="text"
       name="search_string"
       id="search_string"
       v-model="this.search"
-      class="w-full m-2 p-2 bg-gray-100 block rounded-lg"
+      class="w-full -mr-3 m-2 p-2 bg-gray-100 block rounded-lg"
       placeholder="Search for anything"
     />
   </div>
@@ -31,7 +30,7 @@
     </div>
   </div>
   <div class="flex p-1 items-center">
-    <p class="font-medium p-2">Region to train</p>
+    <p class="font-medium p-2">Region:</p>
     <div>
       <Blob
         content="Abs"
@@ -71,7 +70,7 @@
     </div>
   </div>
   <div class="flex p-1 items-center bg-gray-100">
-    <p class="font-medium p-2">Workout type</p>
+    <p class="font-medium p-2">Workout:</p>
     <div>
       <Blob
         content="Warmup"
@@ -125,92 +124,80 @@
       />
     </div>
   </div>
-  <div class="flex flex-col">
-    <div class="my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-        <div
-          class="overflow-hidden border-b border-gray-200 sm:rounded-lg"
-        >
-          <table class="min-w-full divide-y divide-gray-200">
-            <thead class="bg-gray-50">
-              <tr>
-                <th scope="col" class="py-3 px-0 font-medium text-gray-500">
-                  Live
-                </th>
-                <th
-                  scope="col"
-                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Title
-                </th>
-                <th
-                  scope="col"
-                  class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Difficulty / Equipment
-                </th>
-                <th scope="col" class="relative px-6 py-3">
-                  <span class="sr-only">Edit</span>
-                </th>
-              </tr>
-            </thead>
-            <tbody class="bg-white divide-y divide-gray-200">
-              <tr v-for="item in filteredItems" :key="item.id">
-                <td>
-                  <div v-if="item.live" class="w-4 h-20 bg-red-300"></div>
-                </td>
-                <td class="p-0 m-0 whitespace-nowrap">
-                  <div class="flex items-center">
-                    <div class="flex-shrink-2 w-36">
-                      <a :href="item.link">
-                        <!-- <img :src="getImagePath(item.id)" :alt="item.id" /> -->
-                        <img :alt="item.id" />
-                      </a>
-                    </div>
-                    <div class="ml-4">
-                      <div class="text-sm font-medium text-gray-900">
-                        {{ item.heading }}
-                      </div>
-                      <div class="text-sm text-gray-500">
-                        {{ item.subtitle }}
-                      </div>
-                    </div>
-                  </div>
-                </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div>
-                    <div
-                      v-if="item.difficulty_beginner"
-                      class="text-sm rounded-full bg-green-100 text-gray-500 inline-flex px-1.5"
-                    >
-                      beginner
-                    </div>
-                    <div
-                      v-if="item.difficulty_medium"
-                      class="text-sm rounded-full bg-yellow-100 text-gray-600 inline-flex px-1.5"
-                    >
-                      medium
-                    </div>
-                    <div
-                      v-if="item.difficulty_intense"
-                      class="text-sm rounded-full bg-red-100 text-gray-800 inline-flex px-1.5"
-                    >
-                      intense
-                    </div>
-                  </div>
+  <div
+    class="py-2 rounded-lg min-w-full px-6 overflow-x-auto -mx-6"
+  >
+    <table class="divide-y divide-gray-200 min-w-full">
+      <thead class="bg-gray-100">
+        <tr>
+          <th scope="col" class="py-3 px-0 font-medium text-gray-500">🔴</th>
+          <th scope="col" class="text-left text-gray-500 tracking-wider">
+            Link
+          </th>
+          <th scope="col" class="text-left text-gray-500 tracking-wider">
+            Title
+          </th>
+          <th scope="col" class="text-left text-gray-500 tracking-wider">
+            Info
+          </th>
+        </tr>
+      </thead>
+      <tbody class="bg-white divide-y divide-gray-200">
+        <tr v-for="item in filteredItems" :key="item.id">
+          <td>
+            <div v-if="item.live" class="w-4 h-20 bg-red-300"></div>
+          </td>
+          <td class="p-0 m-0 whitespace-nowrap">
+            <div class="flex items-center">
+              <div class="flex-shrink-2 w-36">
+                <a :href="item.link">
+                  <!-- <img :src="getImagePath(item.id)" :alt="item.id" /> -->
+                  <img :alt="item.id" />
+                </a>
+              </div>
+            </div>
+          </td>
+          <td>
+            <div class="ml-4">
+              <div class="text-sm font-medium text-gray-900">
+                {{ item.heading }}
+              </div>
+              <div class="text-sm text-gray-500">
+                {{ item.subtitle }}
+              </div>
+            </div>
+          </td>
+          <td class="px-6 py-4 whitespace-nowrap">
+            <div>
+              <div
+                v-if="item.difficulty_beginner"
+                class="text-sm rounded-full bg-green-100 text-gray-500 inline-flex px-1.5"
+              >
+                beginner
+              </div>
+              <div
+                v-if="item.difficulty_medium"
+                class="text-sm rounded-full bg-yellow-100 text-gray-600 inline-flex px-1.5"
+              >
+                medium
+              </div>
+              <div
+                v-if="item.difficulty_intense"
+                class="text-sm rounded-full bg-red-100 text-gray-800 inline-flex px-1.5"
+              >
+                intense
+              </div>
+            </div>
 
-                  <div
-                    class="text-sm rounded-full bg-blue-100 text-green-800 inline-flex px-1.5"
-                  >
-                    {{ item.equipment }}
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
+            <div
+              class="text-sm rounded-full bg-blue-100 text-green-800 inline-flex px-1.5"
+            >
+              {{ item.equipment }}
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -317,10 +304,7 @@ export default {
           waist: "default",
         },
       },
-      propsToSearch: [
-        "full_title",
-        "scenery",
-      ],
+      propsToSearch: ["full_title", "scenery"],
       items: db_items,
     };
   },
